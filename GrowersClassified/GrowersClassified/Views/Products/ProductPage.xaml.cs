9db1856d7aa725ee8bf1a8aac14574382841f84f
lukaskleinhaneveld <sakulkh@gmail.com>
2018-06-26 12:07:16 -0700

    Added border around products, descriptions are now limited to 5 lines and have tailtrunction

--- GrowersClassified/GrowersClassified/Views/Products/ProductPage.xaml.cs
+++ GrowersClassified/GrowersClassified/Views/Products/ProductPage.xaml.cs
@@ -25,8 +25,8 @@ namespace GrowersClassified.Views.Products
             {
                 user.Username = "Test user " + i;
                 products.Add(new Product(user.Username,         // Username
-                    "Test Title" + ",",                         // Title
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + ",",         // Description
+                    "Test Title",                               // Title
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",         // Description
                     "Test City" + ",",                          // City
                     "Test State" + ",",                         // State
                     "Test make" + ",",                          // Make
